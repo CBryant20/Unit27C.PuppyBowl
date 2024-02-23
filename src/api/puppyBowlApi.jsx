d0c653b2c03690cb01db0547e037f3c5a893ede9
Charles Bryant <cwbryant25@yahoo.com>
2024-02-23 09:22:30 -0800

    API_URL updated

--- src/api/puppyBowlApi.jsx
+++ src/api/puppyBowlApi.jsx
@@ -1,8 +1,7 @@
 import { createAPI, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const API_URL =
-  "https://fsa-puppy-bowl.herokuapp.com/api/2401-FSA-ET-WEB-FT-SF-CHARLES/players";
-
+  "https://fsa-puppy-bowl.herokuapp.com/api/2401-FSA-ET-WEB-FT-SF-CHARLES";
 const api = createAPI({
   baseQuery: fetchBaseQuery({
     baseURL: API_URL,
